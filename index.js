6548ba0474dd349219d123713b46fe435c39ba3d
Sugam Shrestha <info@shresthasugam.com.np>
2026-02-15 21:40:15 +0545

    db connection fixed in index.js

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ app.use((req, res) => {
   });
 });
 
-const PORT = config.port;
+const PORT = config.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.log(`Environment: ${config.nodeEnv}`);
+//   console.log(`Environment: ${config.nodeEnv}`);
 });
 
 
