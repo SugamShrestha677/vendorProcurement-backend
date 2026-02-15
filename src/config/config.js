91770587c2bef9e76515accf316155b6be9f7187
Sugam Shrestha <info@shresthasugam.com.np>
2026-02-15 21:33:01 +0545

    index.js and config.js modified for deployment in render

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ const dotenv = require('dotenv').config();
 
 dotenv.config();
 const config = {
-    PORT:process.env.PORT,
+    PORT:process.env.PORT || 8000,
     MONGODB_URI:process.env.MONGODB_URI,
     JWT_SECRET:process.env.JWT_SECRET,
     JWT_EXPIRATION:process.env.JWT_EXPIRATION,
